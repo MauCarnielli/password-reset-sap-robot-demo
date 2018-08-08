2c567826a6436552783a3844a4fcc7f56f309bc5
MauCarnielli <mau.carnielli@gmail.com>
2018-08-08 13:39:59 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -2,15 +2,23 @@
 
 $method = $_SERVER['REQUEST_METHOD'];
 
-if( $method == 'POST'){
+if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 	$id = $json->queryResult->parameters->id;
 	
 	$response = new \stdClass();
-	$response->speech = $id;
-	$response->displayText = $id;
-	$response->source = 'Gzus';
+	
+	if($id != NULL){
+		$response->speech = $id;
+		$response->displayText = $id;
+		$response->source = 'Gzus';
+	}else{
+		$response->speech = 'Não tem ID nisso ai';
+		$response->displayText = 'Mesma coisa do Speech';
+		$response->source = 'Gzus';
+	}
+	
 	echo json_encode($response);
 	
 }else{
