9a2b20afe472084771ba84d7ee5a4882b391877f
MauCarnielli <mau.carnielli@gmail.com>
2018-08-08 13:20:56 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ $method = $_SERVER['REQUEST_METHOD'];
 if( $method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
-	
-	echo $json;
+	$id = $json->queryResult->parameters->id;
+	echo $id;
 	
 }else{
 	echo 'Nem rolou heim';
