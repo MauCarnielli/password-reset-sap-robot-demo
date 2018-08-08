f3b77216a703b66c3e34fee513f301a55eea453a
MauCarnielli <mau.carnielli@gmail.com>
2018-08-08 13:33:40 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -6,7 +6,12 @@ if( $method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 	$id = $json->queryResult->parameters->id;
-	echo $id;
+	
+	$response = new \stdClass();
+	$response->speech = $id;
+	$response->displayText = $id;
+	$response->source = 'Gzus';
+	echo json_encode($response);
 	
 }else{
 	echo 'Nem rolou heim';
