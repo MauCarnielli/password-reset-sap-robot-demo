85cfbe8a605ffb557305cd614745198c08878d02
MauCarnielli <mau.carnielli@gmail.com>
2018-08-08 11:17:58 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,29 +1,5 @@
 <?php 
 
-$method = $_SERVER['REQUEST_METHOD'];
-
-// Process only when method is POST
-if($method == 'POST'){
-	$requestBody = file_get_contents('php://input');
-	$json = json_decode($requestBody);
-
-	$id = $json->queryResult->parameters->id;
-
-	if($id != null){
-		$speech = "Seu ID é: ",id,"?";
-	}else{
-		$speech = "ID não recebido.";
-	}
-
-	$response = new \stdClass();
-	$response->speech = $speech;
-	$response->displayText = $speech;
-	$response->source = "webhook";
-	echo json_encode($response);
-}
-else
-{
-	echo "Method not allowed";
-}
+echo 'Funcionou'
 
 ?>
