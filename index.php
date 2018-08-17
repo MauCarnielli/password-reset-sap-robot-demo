701cec65e7fc10fa56fc17248529a2a3203cb4af
MauCarnielli <mau.carnielli@gmail.com>
2018-08-17 10:03:58 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
-	echo "OU";
+	$method = $_SERVER['REQUEST_METHOD'];
+
+	if($method == 'POST'){
+		$requestBody = file_get_contents('php://input');
+		$json = json_decode($requestBody);
+		$id = $json->queryResult->parameters->id;
+		
+		$response = new \stdClass();
+		
+		if($id != NULL){
+			$response->speech = $id;
+			$response->displayText = $id;
+			$response->source = 'webhook';
+		}else{
+			$response->speech = 'Nao tem ID nisso ai';
+			$response->fulfillmentText = 'Mesma coisa do Speech';
+			$response->source = 'webhook';
+		}
+		
+		echo json_encode($response);
+		
+	}else{
+		echo 'Nem rolou heim';
+	}
 ?>
