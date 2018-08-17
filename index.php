0520064ea031ec2fe4132807db9dff8395c0acf5
MauCarnielli <mau.carnielli@gmail.com>
2018-08-17 10:08:05 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -20,6 +20,21 @@
 		
 		echo json_encode($response);
 		
+		$url = 'https://my-php-tester.herokuapp.com/';
+		$data = array('key1' => 'value1', 'key2' => 'value2');
+		// use key 'http' even if you send the request to https://...
+		$options = array(
+		  'http' => array(
+			'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+			'method'  => 'POST',
+			'content' => http_build_query($data),
+		  ),
+		);
+		$context  = stream_context_create($options);
+		$result = file_get_contents($url, false, $context);
+		echo json_encode($data);
+		var_dump($result);
+		
 	}else{
 		echo 'Nem rolou heim';
 	}
