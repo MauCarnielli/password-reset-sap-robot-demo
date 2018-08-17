f0d905877bf4501c9705500e008f5f9a033dc9fb
MauCarnielli <mau.carnielli@gmail.com>
2018-08-16 23:38:19 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -5,29 +5,7 @@ function recebeJson{
 	$method = $_SERVER['REQUEST_METHOD'];
 
 	if($method == 'POST'){
-		// $requestBody = file_get_contents('php://input');
-		// $json = json_decode($requestBody);
-		// $id = $json->queryResult->parameters->id;
-		
-		// $response = new \stdClass();
-		
-		// if($id != NULL){
-			// $response->speech = $id;
-			// $response->displayText = $id;
-			// $response->source = 'webhook';
-		// }else{
-			// $response->speech = 'Nao tem ID nisso ai';
-			// $response->fulfillmentText = 'Mesma coisa do Speech';
-			// $response->source = 'webhook';
-		// }
-		
-		// echo json_encode($response);
-		
-	// }else{
-		// echo 'Nem rolou heim';
-	// }
-	
-	enviaPost();
+		enviaPost();
 	}else{
 		echo "huuummmm....."
 	}	
