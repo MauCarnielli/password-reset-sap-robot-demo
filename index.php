01cab8a4d8b0630953cc44ee3e8b410a84298b64
MauCarnielli <mau.carnielli@gmail.com>
2018-08-21 14:22:15 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,50 +1,32 @@
-<?php
-	$method = $_SERVER['REQUEST_METHOD']; //Verifica qual o método da requisição
-	if($method == 'POST'){ 
-		$requestBody = file_get_contents('php://input'); //Recebe o conteúdo em JSON da requisição
-		$json = json_decode($requestBody); //Decodifica de JSON para texto	
-		$id = $json->queryResult->parameters->id; //Extrai apenas o ID, seguindo a estrutura de arrays que está no texto.
-		
-		if($id != NULL){
-			
-			$jobData = array( // Conteúdo do POST a ser mandado para o Orchestrator, afim de começar um Job
-				'startInfo'=>array(
-					'ReleaseKey' => 'Inserir chave do processo aqui',
-					'Strategy' => 'All',
-					'RobotIds' => '[]',
-					'NoOfRobots' => 0
-				)
-			); // Referência -> https://orchestrator.uipath.com/v2018.2/reference#section-starting-a-job
-				
-			
-			$context = stream_context_create(array( //Este é o contexto, ou seja, toda a informação que vai ser passada pela requisição
-				'http'=>array(
-					'method' => 'POST', //Neste caso, mandamos uma requisição do tipo POST
-					'header' => 'Content-Type: application/json\r\n', //Qual o conteúdo que está sendo mandado, no caso um JSON
-					'content' => json_encode($jobData) //O conteúdo, codificado em JSON
-				)
-			)); 
-			
-			$req = file_get_contents('url_do_orchestrator/odata/Jobs/UiPath.Server.Configuration.OData.StartJobs', FALSE, $context); //Envia a requisição para o link e armazena na variável a resposta.
-			
-			$responseDialog = array( //Esta variável é a resposta final, ou seja, a que vai para o DialogFlow. A resposta que o usuário vai obter após o processo.
-				'fulfillmentText' => 'ID Recebido é -> '.$id. '. Requisição completa.', //Aqui vai a mensagem que irá aparecer na conversa com o Chatbot
-				'source' => 'webhook'
-			);
-		}
-		
-		else{
-			$responseDialog = array( //Caso o ID não seja alcançado.
-				'fulfillmentText' => 'ID não recebido',
-				'source' => 'webhook'
-			); 
-		}
-		
-		echo $requestBody;
-		//echo json_encode($responseDialog); //Codifica para JSON e manda para o DialogFlow.
-		
-	}
-	else{
-		echo 'Não foi recebido uma requisição do tipo POST';
-	}
+<?php 
+
+function recebeJson{
+	
+	$method = $_SERVER['REQUEST_METHOD'];
+
+	if($method == 'POST'){
+		enviaPost();
+	}else{
+		echo "huuummmm....."
+	}	
+}
+
+function enviaPost{
+	$url = 'http://server.com/path';
+	$data = array('key1' => 'value1', 'key2' => 'value2');
+	// use key 'http' even if you send the request to https://...
+	$options = array(
+	  'http' => array(
+		'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+		'method'  => 'POST',
+		'content' => http_build_query($data),
+	  ),
+	);
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+	var_dump($result);
+}
+
+echo "hey hey hey";
+
 ?>
